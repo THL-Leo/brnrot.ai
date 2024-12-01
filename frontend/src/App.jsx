8243d3b59dbbc93b18f42c22d379202c64cbfd1a
Leo Lee <leolee901115@gmail.com>
2024-12-01 13:02:31 -0800

    added analytics

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 // import APITest from './ApiTest';
 import DegenPurityTest from "./DegenPurityTest";
+import { Analytics } from '@vercel/analytics/react';
+
 
 function App() {
   return (
     <div>
       <DegenPurityTest />
+      <Analytics />
     </div>
   );
 }
